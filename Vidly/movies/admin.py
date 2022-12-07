8d9ec8bc1df3ec490a713191b0336f3c0ed33b7e
Gaurav <gauravsomani52750@gmail.com>
2022-12-07 16:33:59 +0530

    added new field in movies model, registered 2 models which we want to see in admin panel

--- Vidly/movies/admin.py
+++ Vidly/movies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Genre, Movie
 
-# Register your models here.
+# * Register your models here which you want to manage using Admin panel:
+admin.site.register(Genre)
+admin.site.register(Movie)
