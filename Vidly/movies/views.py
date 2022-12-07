76759fcb91060919172c32c98e017c20df4e770b
Gaurav <gauravsomani52750@gmail.com>
2022-12-07 19:07:37 +0530

    created template and rendered for displaying movies

--- Vidly/movies/views.py
+++ Vidly/movies/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
 
 
 def index(req):
-    return HttpResponse("Hello World!")
+    # select * from movies_movie
+    movies = Movie.objects.all()  # from this we get all the movies from db
+    print('movies:', movies)
+    return render(req, 'movies/index.html', {'movies': movies})
